20e5360291e0545f34c9f52ebb6221a4dd63c345
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2021-12-14 22:54:59 -0800

    Remove extra open= prop in dialog example
    I mistakenly added this in reaction to an error

--- src/routes/dialog/index.svelte
+++ src/routes/dialog/index.svelte
@@ -48,7 +48,7 @@
   <Nested on:close={() => (nested = false)} />
 {/if}
 <TransitionRoot show={isOpen} afterLeave={() => console.log("done")}>
-  <Dialog on:close={() => (isOpen = false)} open={isOpen}>
+  <Dialog on:close={() => (isOpen = false)}>
     <div class="fixed z-10 inset-0 overflow-y-auto">
       <div
         class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
