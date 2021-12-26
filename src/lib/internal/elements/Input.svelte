a033e8ca4af0f49882bdb1005215c0ab4421ecb9
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2021-12-26 11:47:20 -0800

    Add <Input> element
    Most likely this is only useful for tests

---
+++ src/lib/internal/elements/Input.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { ActionArray, useActions } from "$lib/hooks/use-actions";
+  import { get_current_component } from "svelte/internal";
+  import { forwardEventsBuilder } from "../forwardEventsBuilder";
+
+  export let use: ActionArray = [];
+  export let el: HTMLInputElement | null = null;
+  const forwardEvents = forwardEventsBuilder(get_current_component());
+</script>
+
+<input bind:this={el} use:useActions={use} use:forwardEvents {...$$restProps} />
+<slot />
