d1940e5d162f75f9af433621fe345cc61625d053
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2021-12-26 22:45:03 -0800

    Add nested dialog tests

---
+++ src/lib/components/dialog/_NestedDialog.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Dialog from "./Dialog.svelte";
+  import DialogOverlay from "./DialogOverlay.svelte";
+
+  export let level = 1;
+  let showChild = false;
+</script>
+
+<Dialog open={true} on:close>
+  <DialogOverlay />
+
+  <div>
+    <p>Level: {level}</p>
+    <button on:click={() => (showChild = true)}>Open {level + 1} a</button>
+    <button on:click={() => (showChild = true)}>Open {level + 1} b</button>
+    <button on:click={() => (showChild = true)}>Open {level + 1} c</button>
+  </div>
+  {#if showChild}
+    <svelte:self on:close={() => (showChild = false)} level={level + 1} />}
+  {/if}
+</Dialog>
---
+++ src/lib/components/dialog/_NestedTestComponent.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import NestedDialog from "./_NestedDialog.svelte";
+  let open = false;
+</script>
+
+<button on:click={() => (open = true)}>Open 1</button>
+{#if open}
+  <NestedDialog on:close={() => (open = false)} />
+{/if}
