08021bbf21443a9e76c9636074b3667031a05731
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2021-12-26 18:07:47 -0800

    Add support for <Form> tag

--- src/lib/internal/elements/index.ts
+++ src/lib/internal/elements/index.ts
@@ -18,6 +18,7 @@ import Dt from "./Dt.svelte";
 import Div from "./Div.svelte";
 import Em from "./Em.svelte";
 import Footer from "./Footer.svelte";
+import Form from "./Form.svelte";
 import H1 from "./H1.svelte";
 import H2 from "./H2.svelte";
 import H3 from "./H3.svelte";
@@ -26,6 +27,7 @@ import H5 from "./H5.svelte";
 import H6 from "./H6.svelte";
 import Header from "./Header.svelte";
 import I from "./I.svelte";
+import Input from "./Input.svelte";
 import Label from "./Label.svelte";
 import Li from "./Li.svelte";
 import Main from "./Main.svelte";
@@ -57,6 +59,7 @@ const components = {
   "div": Div,
   "em": Em,
   "footer": Footer,
+  "form": Form,
   "h1": H1,
   "h2": H2,
   "h3": H3,
@@ -65,6 +68,7 @@ const components = {
   "h6": H6,
   "header": Header,
   "i": I,
+  "input": Input,
   "label": Label,
   "li": Li,
   "main": Main,
