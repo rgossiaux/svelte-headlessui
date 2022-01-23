458efe129f0a221bef1cf916362b9e5bbaad8142
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-01-23 13:18:40 -0800

    npx svelte-add@latest mdsvex && npm install

---
+++ mdsvex.config.js
@@ -0,0 +1,12 @@
+const config = {
+  extensions: [".svelte.md", ".md", ".svx"],
+
+  smartypants: {
+    dashes: "oldschool",
+  },
+
+  remarkPlugins: [],
+  rehypePlugins: [],
+};
+
+export default config;
