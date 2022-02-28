cd279721df526131a7c342950278d047495c9b72
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-02-28 15:21:57 -0800

    Add redirect on home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script context="module" lang="ts">
+  export function load() {
+    return {
+      status: 302,
+      redirect: "docs",
+    };
+  }
+</script>
