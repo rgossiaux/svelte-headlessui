026b1878908e4768414ac5530cad8d9251cafbe2
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-03-06 23:33:39 -0800

    Add TransitionRoot alias for Transition
    Fixes #46

--- src/lib/components/transitions/index.ts
+++ src/lib/components/transitions/index.ts
@@ -1,2 +1,2 @@
 export { default as TransitionChild } from "./TransitionChildWrapper.svelte";
-export { default as Transition } from "./TransitionRoot.svelte";
+export { default as Transition, default as TransitionRoot } from "./TransitionRoot.svelte";
