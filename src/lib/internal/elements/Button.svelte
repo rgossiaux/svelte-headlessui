fd78eca7edd3f13c93cbc8661233cd7fec8de7b8
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2022-03-18 12:42:01 -0700

    Add back a couple element components used in tests

---
+++ src/lib/internal/elements/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { ActionArray } from "$lib/hooks/use-actions";
+  import { useActions } from "$lib/hooks/use-actions";
+  import { get_current_component } from "svelte/internal";
+  import { forwardEventsBuilder } from "../forwardEventsBuilder";
+  export let use: ActionArray = [];
+  export let el: HTMLButtonElement | null = null;
+  const forwardEvents = forwardEventsBuilder(get_current_component());
+</script>
+
+<button bind:this={el} use:useActions={use} use:forwardEvents {...$$restProps}>
+  <slot />
+</button>
