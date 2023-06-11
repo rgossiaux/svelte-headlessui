f81c75214eed8778ccd7fbfa202488c3b61b6153
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2023-06-10 20:37:03 -0700

    Remove unused ManagedListbox test component

--- src/lib/components/listbox/_ManagedListbox.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Listbox from "./Listbox.svelte";
-  export let value: unknown;
-</script>
-
-<Listbox {value} on:change={(e) => (value = e.detail)} on:change>
-  <slot />
-</Listbox>
