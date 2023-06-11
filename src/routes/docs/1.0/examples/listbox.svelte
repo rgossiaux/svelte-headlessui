6bd93554be33fe215ee0fedaeedf646521c5b154
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2023-06-10 18:27:52 -0700

    Fix app.css imports; fix Listbox example

--- src/routes/docs/1.0/examples/listbox.svelte
+++ src/routes/docs/1.0/examples/listbox.svelte
@@ -21,7 +21,7 @@
 </script>
 
 <div class="w-72 fixed top-16">
-  <Listbox value={selected} on:change={(e) => (selected = e.detail)}>
+  <Listbox bind:value={selected}>
     <div class="relative mt-1">
       <ListboxButton
         class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm"
