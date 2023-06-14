5bb21f51322a83864c8020580e6cca53ad41ae60
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2023-06-14 12:10:22 -0700

    Upgrade kit from 1.0.0-next.202 to 1.0.0-next.299
    annoyingly got stuck in dependency hell here because of breaking changes in vite-plugin-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -26,9 +26,6 @@ const config = {
         return !filepath.endsWith(".test.ts");
       },
     },
-
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: "#svelte",
   },
 };
 
